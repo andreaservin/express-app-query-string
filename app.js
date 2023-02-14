cc8cf1b602f368d7a2dff4863d5b82f9993e0bb5
Andrea <andiserm@gmail.com>
2023-02-13 22:04:42 -0300

    Added rute to get params

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
   res.send(`<h1>Hola ${req.query.nombre ?? 'desconocido'}!</h1>`)
 })
 
+app.get('/makers/:nombre', (req, res) => {
+    const { nombre } = req.params
+    res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase() + nombre.slice(1) ?? 'Desconocido'}!</h1>`)
+  })
+
 app.listen(3000, () => console.log('Listening on port 3000!'))
